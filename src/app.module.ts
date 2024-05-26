f90ccac22bbb2f93c0601413734c5df2e6b091c2
Emilio Selmanaj <emilioselmanaj@gmail.com>
2024-05-27 01:28:04 +0300

    Clean up

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { AppConfigModule } from './config/config.module';
 import { allProviders } from './providers';
 import { GameRestController } from './api/rest/game.controller';
 
 @Module({
   imports: [AppConfigModule.register({ envFilePath: '.env.development' })],
-  controllers: [AppController, GameRestController],
-  providers: [AppService, ...allProviders],
+  controllers: [GameRestController],
+  providers: [...allProviders],
 })
 export class AppModule {}
